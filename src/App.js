633b5556921c680a23c5bf924545ccba500b2e97
enji1115 <jonji1115@gmail.com>
2026-09-16 14:47:45 +0900

    React 학습 > State 활용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Greeting from "./components/Greeting";
-import Card from "./components/Card";
+// import Card from "./components/Card";
 
 function App() {
   // logic
@@ -28,13 +28,15 @@ function App() {
       변경~!
 
       <Greeting />
-      <Greeting />
+      {/* <Greeting /> */}
 
       {/* 미션: Card 컴포넌트 만들고, 이 자리에 노출 */}
+      {/*
       <Card />
       <Card />
       <Card />
       <Card />
+      */}
     </div>
   );
 }
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Greeting = () => {
+  // logic
+  // const username = "???"; // 일반 JS - 값 지정
+  // let username = ""; // 일반 JS - 변수 적용
+  const [username, setUsername] = useState("");
+  const handleClick =()=>{
+    // 일반 JS - 변수를 사용하여 사용자 이름 변경(React에서는 적용 안됨!!!)
+    // username = window.prompt("성함이 어떻게 되시나요?");
+
+    // 수정할 값을 인자로 넣어서 state 업데이트
+    setUsername(window.prompt("성함이 어떻게 되시나요?"));
+  }
+
+  // view
   return (
     <div style={{"backgroundColor":"pink","padding":"10px"}}>
-      <p>안녕하세요, 홍길동님!</p>
+      <p>안녕하세요, {username}님!</p>
       <p>오늘도 좋은 하루 보내세요~😄</p>
+      <button type="button" onClick={handleClick}>입력</button>
     </div>
   )
 }
