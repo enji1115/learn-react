042138b850d313cfcec07d8980673471e433b799
enji1115 <jonji1115@gmail.com>
2026-09-16 13:25:06 +0900

    React 학습 > 기초 세팅, 컴포넌트 기초

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import Greeting from "./Greeting";
+
 function App() {
+  // logic
+  // 구조분해할당 - object destructuring
+  // 배열
+  const animals = ["dog", "cat", "pic"];
+  // const second = animals[1];
+  const [patato, second] = animals;
+  console.log("🚀 ~ App ~ first:", patato);
+  console.log("🚀 ~ App ~ second:", second);
+  // 객체
+  const todoItem = {
+    todo: "React학습하기",
+    isComplete: false
+  }
+  // const todo = todoItem.todo;
+  // const isComplete = todoItem.isComplete;
+  const { todo: todoName, isComplete } = todoItem;
+  // console.log("🚀 ~ App ~ todo:", todo);
+  console.log("🚀 ~ App ~ todoName:", todoName);
+  console.log("🚀 ~ App ~ isComplete:", isComplete);
+
+  // view
   return (
     <div className="App">
       변경~!
+      <Greeting />
+      <Greeting />
     </div>
   );
 }
